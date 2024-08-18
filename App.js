351a352cf5621369b7f8d9438e78ac41e7a844c4
Tushar <tusharjma8@gmail.com>
2024-08-18 12:50:38 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
-const heading = React.createElement("div", {className : 'container'},
-            React.createElement('h1', null, 'Hello World!'),
-            React.createElement('p', null, "Today i'm doing a quick start on react. Hope i'll complete my task") 
-    );
+let myElement = React.createElement('h1', null,'Hello my name is tushar');
+let root = ReactDOM.createRoot(document.getElementById('root'))
 
-
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+root.render(myElement);
